fbf7b7ca009ee875d33b3bdc0df51d923be4769b
23NateK <n.kimber22@gmail.com>
2023-03-08 11:37:05 -0600

    Finished parseFromString

--- TimeDuration.java
+++ TimeDuration.java
@@ -39,17 +39,59 @@ public static TimeDuration parseFromString(String s){
     int sec=0;
     int min=0;
     int hour=0;
-int sEndindex = s.indexOf("s");
-int sStartindex = s.indexOf(",")+1;
-while(numInString(s.substring(sStartindex, sStartindex+1))==false){
-    sStartindex++;
+if(s.indexOf("h")!=-1){
+int hEndindex = s.indexOf("h");
+int hStartindex = s.lastIndexOf(",",hEndindex)+1;
+
+while(numInString(s.substring(hStartindex, hStartindex+1))==false){
+    hStartindex++;
 }
-if(sEndindex!=-1){
-    while(numInString(s.substring(sEndindex, sEndindex+1))==false){
-        sEndindex--;
+if(hEndindex!=-1){
+    while(numInString(s.substring(hEndindex, hEndindex+1))==false){
+        hEndindex--;
     }
 }
-sec=Integer.parseInt(s.substring(sStartindex, sEndindex+1));
+hour=Integer.parseInt(s.substring(hStartindex, hEndindex+1));
+}
+if(s.indexOf("m")!=-1){
+    int mEndindex = s.indexOf("m");
+    int mStartindex = s.lastIndexOf(",",mEndindex)+1;
+
+    while(numInString(s.substring(mStartindex, mStartindex+1))==false){
+        mStartindex++;
+    }
+    if(mEndindex!=-1){
+        while(numInString(s.substring(mEndindex, mEndindex+1))==false){
+            mEndindex--;
+        }
+    }
+    min=Integer.parseInt(s.substring(mStartindex, mEndindex+1));
+}
+if(s.indexOf("s")!=-1){
+    int sEndindex = s.indexOf("s");
+    int sStartindex = s.lastIndexOf(",",sEndindex)+1;
+    while(numInString(s.substring(sStartindex, sStartindex+1))==false){
+        sStartindex++;
+    }
+    if(sEndindex!=-1){
+        while(numInString(s.substring(sEndindex, sEndindex+1))==false){
+            sEndindex--;
+        }
+    }
+    sec=Integer.parseInt(s.substring(sStartindex, sEndindex+1));
+}
+if(s.indexOf(":")!=-1){
+    String[] s2=s.split(":");
+    if(s2.length==2){
+        min=Integer.parseInt(s2[0]);
+        sec=Integer.parseInt(s2[1]);
+    }else{
+        hour=Integer.parseInt(s2[0]);
+        min=Integer.parseInt(s2[1]);
+        sec=Integer.parseInt(s2[2]);
+    }
+
+}
 
     return new TimeDuration(hour, min, sec);
 }
@@ -73,7 +115,7 @@ private static boolean numInString(String s){
     public static void main(String[] args) {
         //TimeDuration time = new TimeDuration(7,2,4);
         //System.out.println(time.toString());
-    String input="5 s";
+    String input="12:20:13";
     TimeDuration time2=parseFromString(input);
     System.out.println(time2.toString());
     }
